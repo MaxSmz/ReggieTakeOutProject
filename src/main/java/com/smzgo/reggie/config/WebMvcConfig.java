3d8050eeea764582a05e2806ff1b4c8df108f965
Rnichts Shune <jasonsun@Mingzes-MacBook-Pro.local>
2023-02-23 14:20:19 -0800

    Implement update function in controller, add JacksonObjectMapper and extend MVC message converters

--- src/main/java/com/smzgo/reggie/config/WebMvcConfig.java
+++ src/main/java/com/smzgo/reggie/config/WebMvcConfig.java
@@ -1,15 +1,24 @@
 package com.smzgo.reggie.config;
 
+import com.smzgo.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 
@@ -17,5 +26,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
 
+    }
+
+
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器...");
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，底层使用Jackson将Java对象转换成了json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将新设置的转换器追加到mvc框架的转换器集合中
+        converters.add(0,messageConverter);
     }
 }
